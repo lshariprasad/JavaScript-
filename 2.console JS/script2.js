a272f0d3690e5d0b6ed7fdbf17f4fa40500a4ba4
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-10-20 17:35:04 +0530

    Console JS
    Just Inspect Your Webpage - They You Find The Console Tab.
    Your Script Are Working On That Place.
    Like Seeing Output.

---
+++ 2.console JS/script2.js
@@ -0,0 +1,23 @@
+//alert("Hey Here, Welcome to Console JS file");
+
+// || Console Methods || //
+console.log("Welcome to DEV Community");
+console.log(1410);
+console.log(20.0);
+console.log(true);
+console.log(false);
+console.log([22, 99, 32, 64, 88, 100]);
+console.log({ name: "DEV", age: 20, country: "India" });
+console.table({ name: "SRI", age: 20, country: "India" });
+console.warn("You Are Under My Control");
+console.error("You Camera Is Error, Clean It Now!");
+console.clear();
+
+console.info("Krishna, I Like You A Lot");
+
+console.time("Timer");
+
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+console.timeEnd("Timer");
