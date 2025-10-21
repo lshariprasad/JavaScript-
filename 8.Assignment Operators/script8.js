7e0da034ab70d405811fc65b6ddff0c4b037c727
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-10-21 15:02:49 +0530

    Assignment Operators

---
+++ 8.Assignment Operators/script8.js
@@ -0,0 +1,22 @@
+// || Assignment Operators in JS || //
+
+let a = 10;
+let b = 5;
+
+a += b; // a = a + b
+console.log("a += b: ", a);
+
+a -= b; // a = a - b
+console.log("a -=b:", a);
+
+a *= b; // a = a*b;
+console.log("a *=b:", a);
+
+a /= b; // a = a/b;
+console.log("a /=b:", a);
+
+a %= b; // a = a%b;
+console.log("a %=b:", a);
+
+a **= b; // a = a**b;
+console.log("a**b:", a);
