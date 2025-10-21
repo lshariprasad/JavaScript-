7299adf47f24e8be27e19f87d8c9c4475f9c5c55
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-10-21 15:03:54 +0530

    Logical Operators

---
+++ 10.Logical Operators/script10.js
@@ -0,0 +1,18 @@
+// || Logical Operator || //
+/* 
+&& logical and
+|| logical or
+!  logical not
+*/
+
+let mark = 95;
+
+console.log(mark >= 35 && mark <= 100);
+
+let a = 10;
+
+console.log(a == 2 || a == 5);
+
+b = true;
+
+console.log(!b);
