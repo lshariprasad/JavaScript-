e43757f1a1cbe53dac172b957daa8b9ff57b9a44
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-10-21 15:02:19 +0530

    Arithmetic Operation JS

---
+++ 7.Arithmetic Operation JS/script7.js
@@ -0,0 +1,35 @@
+// || Arithmetic Operators in JS || //
+
+let a = 14;
+let b = 18;
+let c;
+
+c = a + b;
+console.log("Addition: ", c);
+
+c = a - b;
+console.log("subtraction: ", c);
+
+c = a * b;
+console.log("multiplication: ", c);
+
+c = a / b;
+console.log("division: ", c);
+
+c = a % b;
+console.log("modulus: ", c);
+
+c = a ** b;
+console.log("exponentiation: ", c);
+
+c = ++a;
+console.log("pre-increment: ", c);
+
+c = --b;
+console.log("pre-decrement:", c);
+
+c = a++;
+console.log("post-increment:", c);
+
+c = b--;
+console.log("post-decrement: ", c);
