1e17d81d4bdcb9d8f8c75052e5ca75da0418008b
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-10-21 15:03:30 +0530

    Comparison and Relation Operators JS

---
+++ 9.Comparison and Relation Operators JS/script9.js
@@ -0,0 +1,15 @@
+// || Comparison Operators in JS || //
+
+let a = 100;
+let b = 200;
+
+console.log("a == b:", a == b); // equal to
+console.log("a === b:", a === b); // strict equal to
+console.log("a != b:", a != b); // not equal to
+console.log("a !== b:", a !== b); // strict not equal to
+
+// || Relational Operators in JS || //
+console.log("a > b: ", a > b); // greater than
+console.log("a < b : ", a < b); // less than
+console.log("a >= b", a >= b); // greater than or equal to
+console.log("a <= b", a <= b); // less than or equal to
