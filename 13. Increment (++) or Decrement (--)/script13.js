7a5950e285d91b5bb3bed93eaa17aad0778fb32a
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-10-22 09:06:43 +0530

    Increment (++) or Decrement (--)

---
+++ 13. Increment (++) or Decrement (--)/script13.js
@@ -0,0 +1,21 @@
+// Increment (++) Operator or Decrement (--) Operator
+
+let a = 1;
+a++;
+console.log(a); // 2
+
+let b = 1;
+b--;
+console.log(b); // 0
+
+let c = 5;
+++c;
+console.log(c); // 6
+
+let d = 5;
+--d;
+console.log(d); // 4
+
+let e = 10;
+console.log(e++); // 10 (prints e, then increments)
+console.log(e); // 11 (e has been incremented)
