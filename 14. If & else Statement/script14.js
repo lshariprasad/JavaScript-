c4c865dc0cbd454eb45d31d676058d1e76573afd
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-10-22 09:07:08 +0530

    If & else Statement

---
+++ 14. If & else Statement/script14.js
@@ -0,0 +1,8 @@
+// If Statement Example //
+
+let age = prompt("Please enter your age:");
+if (age != null && age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote.");
+}
