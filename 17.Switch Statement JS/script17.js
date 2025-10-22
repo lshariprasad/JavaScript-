38a26ee84efdce3d66b91ea14803c9038ee428c4
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-10-22 09:08:25 +0530

    Switch Statement JS

---
+++ 17.Switch Statement JS/script17.js
@@ -0,0 +1,20 @@
+// Switch Statement //
+
+// Example 1 //
+
+let number = Number(prompt("Enter The Number : "));
+
+switch (number) {
+  case 1:
+    console.log("One");
+    break;
+  case 2:
+    console.log("Two");
+    break;
+  case 3:
+    console.log("Three");
+    break;
+  default:
+    console.log("Invalid Input");
+    break;
+}
