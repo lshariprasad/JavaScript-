920536aed0426cefc2d88491b92aff4c7c6011a2
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-10-23 07:36:24 +0530

    Math Function JS

---
+++ 27.Math Function JS/script27.js
@@ -0,0 +1,24 @@
+// Math Function //
+
+let c;
+c = Math.PI;
+c = Math.E;
+c = Math.round(5.8);
+c = Math.round(5.3);
+c = Math.floor(5.3);
+c = Math.ceil(5.3);
+c = Math.sqrt(9);
+c = Math.abs(-96);
+c = Math.trunc(25.58);
+c = Math.pow(25, 2);
+c = Math.max(25, 2, 100, 1000, 2505, 1025);
+c = Math.min(25, 2, 100, 1, 0.1, 2.2, 6);
+c = Math.random();
+c = Math.floor(Math.random() * 50 + 1);
+c = Math.sign(25);
+c = Math.sin(90);
+c = Math.cos(45);
+c = Math.log(45);
+c = Math.log2(25);
+
+console.log(c);
