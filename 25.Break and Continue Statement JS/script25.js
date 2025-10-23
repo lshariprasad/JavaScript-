1ab3412636b6c4e24c332ebee3b2e0ab791c1af0
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-10-23 07:35:39 +0530

    Break and Continue Statement JS

---
+++ 25.Break and Continue Statement JS/script25.js
@@ -0,0 +1,23 @@
+// || Break || //
+for (let i = 1; i < 10; i++) {
+  console.log(i);
+  if (i == 4) {
+    break;
+  }
+}
+
+// || Continue || //
+for (let i = 1; i < 10; i++) {
+  if (i == 4) {
+    continue;
+  }
+  console.log(i);
+}
+
+// || EXAMPLE-1 || //
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 0) {
+    continue;
+  }
+  console.log(i);
+}
