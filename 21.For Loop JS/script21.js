9b9989e948ecb45ef655ef486bef1990387e775a
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-10-23 07:33:37 +0530

    For Loop JS

---
+++ 21.For Loop JS/script21.js
@@ -0,0 +1,13 @@
+// For Loop JS
+
+let limits = Number(prompt("Enter Your Limits: "));
+
+for (let i = 1; i <= limits; i++) {
+  console.log(i);
+}
+
+let arr = [];
+for (let i = 0; i <= 100; i++) {
+  arr.push(i);
+}
+console.log(arr);
