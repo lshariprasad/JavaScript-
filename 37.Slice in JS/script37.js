0b727c8095b07400f5547308933d6b584d662a8c
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-25 10:09:27 +0530

    Slice in JS

---
+++ 37.Slice in JS/script37.js
@@ -0,0 +1,7 @@
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//slice(start,end)
+console.log(number);
+console.log("Slice :" + number.slice());
+console.log("Slice(2) :" + number.slice(2));
+console.log("Slice(2) :" + number.slice(2, 5));
