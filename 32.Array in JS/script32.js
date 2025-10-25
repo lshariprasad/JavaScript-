e73793413526aa9c2fb2b748757ef550a2f07f16
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-25 07:06:51 +0530

    Array in JS

---
+++ 32.Array in JS/script32.js
@@ -0,0 +1,10 @@
+let a = [10, 20, 30, 40, 50];
+console.log(a);
+console.table(a);
+console.log(a[1]);
+
+let b = new Array(10, 20, 30, 40, 50);
+console.table(b);
+
+let c = new Array("Dev", 10, 20, true, 30, 40, 50, { m1: 100, m2: 95, m3: 89 });
+console.table(c);
