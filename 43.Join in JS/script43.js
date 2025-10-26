242ed709736528f4f9e243ce665629c5545ba0f2
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-26 06:27:08 +0530

    Join in JS

---
+++ 43.Join in JS/script43.js
@@ -0,0 +1,6 @@
+//array.join (separator)
+const products = ["car", "money", "building", "cash"];
+console.log(products);
+
+console.log(products.join()); //Deafult , as separator
+console.log(products.join("|")); //Deafult | as separator
