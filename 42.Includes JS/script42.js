46d4486b49bf739dd50d80566af8f6af9e220717
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-26 06:26:50 +0530

    Includes JS

---
+++ 42.Includes JS/script42.js
@@ -0,0 +1,11 @@
+//Includes(Value,start_indedx)
+const products = ["car", "money", "building", "cash"];
+
+let result = products.includes("car");
+console.log(result);
+
+result = products.includes("girls");
+console.log(result);
+
+result = products.includes("pen", 2);
+console.log(result);
