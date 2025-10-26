891b349e03da56433515e90be27f6594def8085b
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-26 08:04:21 +0530

    LastindexOf in JS

---
+++ 50.LastindexOf in JS/script50.js
@@ -0,0 +1,23 @@
+students = [
+  "Navya",
+  "Sri",
+  "Vasavi",
+  "Dev",
+  "Vasavi",
+  "Sathish",
+  "Hari",
+  "Vijay",
+  "Anand",
+];
+
+let i = students.indexOf("Vasavi");
+console.log(i);
+
+i = students.lastIndexOf("Vasavi");
+console.log(i);
+
+let address = " Dev Krishna Mangadu Chennai krishna";
+i = address.indexOf("krishna");
+console.log(i);
+i = address.lastIndexOf("krishna");
+console.log(i);
