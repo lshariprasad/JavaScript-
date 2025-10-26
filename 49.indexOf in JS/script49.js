c2d24a2963deeae7f94fbde2fc83a282f959fcec
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-26 08:03:19 +0530

    indexOf in JS

---
+++ 49.indexOf in JS/script49.js
@@ -0,0 +1,20 @@
+student = [
+  "Navya",
+  "Sri",
+  "Vasavi",
+  "Dev",
+  "Sathish",
+  "Hari",
+  "Vijay",
+  "Anand",
+];
+
+let i = student.indexOf("Sri");
+console.log("Index :", i);
+
+let user = "Deva Krishna";
+let index = user.indexOf("a");
+console.log("Index :", +index);
+
+index = user.indexOf("a", 4);
+console.log("Index :", +index);
