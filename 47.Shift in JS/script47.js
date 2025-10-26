3082d253ee26661cdef31b182f6d1db69e394084
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-26 08:02:12 +0530

    Shift in JS

---
+++ 47.Shift in JS/script47.js
@@ -0,0 +1,13 @@
+// Shift()
+
+let student = ["Vasavi", "Dev", "Sri", "Hari", "Sathish", "Vijay"];
+console.log("Before Shift : " + student);
+
+let element = student.shift(); // removes the first element
+console.log("After Shift : " + student);
+console.log("Removed Element : " + element);
+
+console.log("Before Shift : " + student);
+element = student.shift(); // removes the next first element
+console.log("After Shift : " + student);
+console.log("Removed Element : " + element);
