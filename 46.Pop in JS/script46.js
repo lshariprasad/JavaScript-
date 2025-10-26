249cf9366bb1ff09a0ece0bd3393869aa5c43c05
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-26 08:01:53 +0530

    Pop in JS

---
+++ 46.Pop in JS/script46.js
@@ -0,0 +1,8 @@
+//Pop in Javascript.
+
+const users = ["Dev", "Hari", "Swetha", "Sathish", "Vijay"];
+console.log(users);
+console.log(users.pop());
+console.log(users);
+console.log(users.pop());
+console.log(users);
