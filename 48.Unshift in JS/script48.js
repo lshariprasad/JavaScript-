43aefaf69dd2953c175d8e4e8d898b7766363c9d
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-26 08:02:56 +0530

    Unshift in JS

---
+++ 48.Unshift in JS/script48.js
@@ -0,0 +1,14 @@
+//Unshift()
+//Add First Element at Start
+
+student = ["Dev", "Sathish", "Hari", "Vijay", ""];
+console.log("Before Unshift : " + student);
+
+let len = student.unshift("Vasavi");
+console.log("Length : " + len);
+console.log("After Unshift : " + student);
+
+//Mulitiple Values
+len = student.unshift("Navya", "Sri");
+console.log("Length : " + len);
+console.log("After Unshift : " + student);
