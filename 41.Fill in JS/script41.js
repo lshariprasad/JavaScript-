164a8d55be2b9b91087f08957f7147f2de972fae
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-26 06:26:24 +0530

    Fill in JS

---
+++ 41.Fill in JS/script41.js
@@ -0,0 +1,13 @@
+//Fill(value,start,end)
+
+let n = [1, 2, 3, 4, 5, 6];
+
+console.log("Before Fill : " + n);
+n.fill(20);
+console.log("After Fill : " + n);
+
+n.fill("a");
+console.log("After Fill : " + n);
+
+n.fill(20, 3, 5);
+console.log("After Fill : " + n);
