a85c4aaa75c3d18aeea8da0840da1555fd86e14c
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-27 09:04:10 +0530

    Multiple ways to clone an array in JS

---
+++ 53. Multiple ways to clone an array in JS/script53.js
@@ -0,0 +1,19 @@
+// Multiple Ways to clone an array in JavaScript
+
+let originalArray = [1, 2, 3];
+/* let clonedArray = [...originalArray];
+
+//Slice()
+let clonedArray = originalArray.slice();
+
+//concat
+let clonedArray = [].concat(originalArray);
+
+//Array.from()
+let clonedArray = Array.from(originalArray);
+
+//JSON.parse(),JSON.stringify()
+let clonedArray = JSON.parse(JSON.stringify(originalArray));
+console.log(clonedArray);
+
+ */
