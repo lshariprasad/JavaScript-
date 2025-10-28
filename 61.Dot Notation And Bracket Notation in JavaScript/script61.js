d13a052fc09589d302ebbafe1fbe08c14035646a
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-29 05:13:06 +0530

    Dot Notation And Bracket Notation in JavaScript

---
+++ 61.Dot Notation And Bracket Notation in JavaScript/script61.js
@@ -0,0 +1,36 @@
+//Dot Notation And Bracket Notation in JS
+/* 
+const user = {
+  name: "Dev",
+  age: 19,
+  job: "Developer",
+};
+
+console.log(user);
+
+// Dot notation
+console.log(user.name);
+user.name = "Sri";
+console.log(user.name);
+console.log(user);
+
+//Bracket Notation
+console.log(user["age"]);
+user["age"] = 20;
+console.log(user["age"]);
+console.log(user);
+
+const prop = "name";
+console.log(user[prop]);
+
+ */
+
+let user = {
+  "first name": "Deva",
+  "last name": "Krishna",
+};
+console.log(user["first name"]);
+console.log(user["last name"]);
+
+let key = "first name";
+console.log(user[key]);
