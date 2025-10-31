ea00f917e8a0defa72aaa85cc3a7718d80b17403
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-10-31 08:13:22 +0530

    Prototype

---
+++ 71.Prototype/script71.js
@@ -0,0 +1,12 @@
+/* let array = ["apple", "orange"];
+
+let object = {
+  name: "Hari",
+  city: "Chennai",
+  info: function () {
+    return `${this.name} from ${this.city}`;
+  },
+};
+
+function myFunction() {}
+ */
